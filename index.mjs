43cb36de6083ca0fa6b31e7ccc1ef8f4f4b553f4
PonomareVlad <Git@PonomareVlad.ru>
2020-11-27 15:27:12 +0500

    [Prototype] Saving user answers to LocalStorage

--- index.mjs
+++ index.mjs
@@ -2,6 +2,7 @@ export default class Quiz {
     endpointURL = 'https://woodstone-dev-app.herokuapp.com/api/';
 
     constructor() {
+        this.loadAnswers();
         this.init();
         return this;
     }
@@ -18,10 +19,9 @@ export default class Quiz {
 
     async renderQuiz() {
         const element = document.createElement('quiz-page');
-        element.id = 'quiz';
-        element.innerHTML = `<h1>Конфигуратор</h1>`;
+        element.innerHTML = `<h1>Конфигуратор</h1><form id="quiz"></form>`;
         document.body.appendChild(element);
-        return await this.quizSteps.forEach(await this.renderStep);
+        return await this.quizSteps.forEach(await this.renderStep, this);
     }
 
     async renderStep(step) {
@@ -31,20 +31,49 @@ export default class Quiz {
         switch (step.typeId) {
             case 1:
                 step.data.counters.forEach(item => element.innerHTML += `<div><label for="${item.fieldname}">${item.titie}</label>
-<input id="${item.fieldname}" name="${item.fieldname}" type="number" value="0"></div>`);
+<input id="${item.fieldname}" name="${item.fieldname}" type="number" value="${this.getAnswer(step.data.fieldname, item.fieldname, 0)}" onchange="app.setAnswer('${step.data.fieldname}','${item.fieldname}',parseInt(this.value))"></div>`);
                 break;
             case 2:
                 step.data.ranges.forEach(item => element.innerHTML += `<div><label for="${item.fieldname}">${item.titie}</label>
-<input id="${item.fieldname}" name="${item.fieldname}" type="range" min="${item.minValue}" max="${item.maxValue}"></div>`);
+<input id="${item.fieldname}" name="${item.fieldname}" type="range" min="${item.minValue}" max="${item.maxValue}" value="${this.getAnswer(step.data.fieldname, item.fieldname, item.minValue)}" onchange="app.setAnswer('${step.data.fieldname}','${item.fieldname}',parseInt(this.value))"></div>`);
                 break;
             case 3:
-                step.data.options.forEach(item => element.innerHTML += `<div><input id="${step.data.fieldname}_${item.value}" name="${step.data.fieldname}" value="${item.value}" type="radio">
+                const checkedValue = this.getAnswer(step.data.fieldname, false, 0);
+                step.data.options.forEach(item => element.innerHTML += `<div><input ${item.value === checkedValue ? 'checked' : ''} id="${step.data.fieldname}_${item.value}" name="${step.data.fieldname}" value="${item.value}" type="radio" onchange="app.setAnswer('${step.data.fieldname}',false,parseInt(this.form.${step.data.fieldname}.value))">
 <label for="${step.data.fieldname}_${item.value}">${item.titie}</label></div>`);
                 break;
             default:
                 return false;
         }
         document.getElementById('quiz').appendChild(element);
+    }
+
+    setAnswer(step, item, value) {
+        if (!item) this.preferences[step] = value;
+        else {
+            if (!this.preferences[step]) this.preferences[step] = {}
+            this.preferences[step][item] = value;
+        }
+        this.saveAnswers();
+    }
+
+    getAnswer(step, item, defaultValue = '') {
+        if (!item) return this.preferences[step] ? this.preferences[step] : defaultValue;
+        if (!this.preferences[step]) return defaultValue;
+        return this.preferences[step][item] ? this.preferences[step][item] : defaultValue;
+    }
+
+    saveAnswers() {
+        localStorage.setItem('preferences', JSON.stringify(this.preferences));
+    }
+
+    loadAnswers() {
+        try {
+            this.preferences = localStorage.getItem('preferences') ? JSON.parse(localStorage.getItem('preferences')) : {}
+        } catch (e) {
+            console.error(e);
+            this.preferences = {}
+        }
     }
 
 }
