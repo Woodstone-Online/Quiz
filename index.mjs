884d462c4e1c70719e57c848f4444329c6c5f752
PonomareVlad <Git@PonomareVlad.ru>
2020-11-28 16:06:58 +0500

    [Prototype] Fixed "titie"

--- index.mjs
+++ index.mjs
@@ -30,17 +30,17 @@ export default class Quiz {
         element.innerHTML += `<h2>${step.title}</h2>`;
         switch (step.typeId) {
             case 1:
-                step.data.counters.forEach(item => element.innerHTML += `<div><label for="${item.fieldname}">${item.titie}</label>
+                step.data.counters.forEach(item => element.innerHTML += `<div><label for="${item.fieldname}">${item.title}</label>
 <input id="${item.fieldname}" name="${item.fieldname}" type="number" value="${this.getAnswer(step.data.fieldname, item.fieldname, 0)}" onchange="app.setAnswer('${step.data.fieldname}','${item.fieldname}',parseInt(this.value))"></div>`);
                 break;
             case 2:
-                step.data.ranges.forEach(item => element.innerHTML += `<div><label for="${item.fieldname}">${item.titie}</label>
+                step.data.ranges.forEach(item => element.innerHTML += `<div><label for="${item.fieldname}">${item.title}</label>
 <input id="${item.fieldname}" name="${item.fieldname}" type="range" min="${item.minValue}" max="${item.maxValue}" value="${this.getAnswer(step.data.fieldname, item.fieldname, item.minValue)}" onchange="app.setAnswer('${step.data.fieldname}','${item.fieldname}',parseInt(this.value))"></div>`);
                 break;
             case 3:
                 const checkedValue = this.getAnswer(step.data.fieldname, false, 0);
                 step.data.options.forEach(item => element.innerHTML += `<div><input ${item.value === checkedValue ? 'checked' : ''} id="${step.data.fieldname}_${item.value}" name="${step.data.fieldname}" value="${item.value}" type="radio" onchange="app.setAnswer('${step.data.fieldname}',false,parseInt(this.form.${step.data.fieldname}.value))">
-<label for="${step.data.fieldname}_${item.value}">${item.titie}</label></div>`);
+<label for="${step.data.fieldname}_${item.value}">${item.title}</label></div>`);
                 break;
             default:
                 return false;
