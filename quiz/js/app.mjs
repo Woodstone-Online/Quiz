c12be02387f7e9b76eb11b1cc22116f805ba337d
PonomareVlad <Git@PonomareVlad.ru>
2021-01-06 23:56:32 +0500

    [Quiz] Styles update

--- quiz/js/app.mjs
+++ quiz/js/app.mjs
@@ -1,95 +1,45 @@
-import quiz from './quiz.mjs'
+import quiz, {loadStyles} from './quiz.mjs'
 import {LitElement, html, css} from 'https://jspm.dev/lit-element';
 import './menu.mjs';
 import './needs.mjs';
 import './location.mjs';
+import './home.mjs';
 import './contacts.mjs';
 
-window.customElements.define('quiz-app', class extends LitElement {
-    constructor() {
-        super();
-        // this.updateStage();
-        window.addEventListener('hashchange', this.updateStage.bind(this));
-    }
-
-    static get properties() {
-        return {
-            stage: {type: String}
+loadStyles(import.meta.url).then(styles =>
+    window.customElements.define('quiz-app', class extends LitElement {
+        constructor() {
+            super();
+            // this.updateStage();
+            window.addEventListener('hashchange', this.updateStage.bind(this));
         }
-    }
 
-    updateStage() {
-        let targetStage = location.hash.substr(1)
-        if (targetStage && document.getElementById(targetStage)) return this.stage = targetStage;
-        return location.hash = 'needs';
-    }
-
-    static get styles() {
-        return css`
-          * {
-            touch-action: manipulation;
-            -webkit-text-size-adjust: none;
-          }
-
-          :host {
-            flex-direction: column;
-            //background: #F2F4F6;
-            max-height: 100vh;
-            max-width: 100vw;
-            display: flex;
-            height: 100%;
-            width: 100%;
-          }
-
-          main {
-            display: initial;
-            flex: 1;
-            max-height: 100%;
-            overflow-y: scroll;
-            padding-top: 15px;
-            margin-top: -15px;
-          }
-
-          @media (min-width: 1024px) {
-            main {
-              padding: 0 30px;
-              padding-bottom: 30px;
-              margin: initial;
+        static get properties() {
+            return {
+                stage: {type: String}
             }
-          }
+        }
 
-          @media (min-width: 1364px) {
-            :host {
-              flex-direction: row;
-              justify-content: space-between;
-              //background: #F5F6F8;
-            }
+        static get styles() {
+            return css([styles]);
+        }
 
-            quiz-menu {
-              flex-grow: 1;
-            }
+        updateStage() {
+            let targetStage = location.hash.substr(1)
+            if (targetStage && document.getElementById(targetStage)) return this.stage = targetStage;
+            return location.hash = 'needs';
+        }
 
-            main {
-              max-width: 1024px;
-              min-width: 1024px;
-              width: 1024px;
-              margin: initial;
-              padding: 30px;
-            }
-          }
-        `;
-    }
+        /*firstUpdated() {
+            // quiz.initRouter(this.shadowRoot.querySelector('main'));
+            this.shadowRoot.insertBefore(document.createElement('quiz-menu'), this.shadowRoot.querySelector('main'));
+        }*/
 
-    /*firstUpdated() {
-        // quiz.initRouter(this.shadowRoot.querySelector('main'));
-        this.shadowRoot.insertBefore(document.createElement('quiz-menu'), this.shadowRoot.querySelector('main'));
-    }*/
-
-    render() {
-        return html`
-            <!--<quiz-menu></quiz-menu>-->
-            <slot></slot>
-            <main>${this.stage ? document.createElement('quiz-' + this.stage) : null}</main>
-        `;
-    }
-});
+        render() {
+            return html`
+                <!--<quiz-menu></quiz-menu>-->
+                <slot></slot>
+                <main>${this.stage ? document.createElement('quiz-' + this.stage) : null}</main>
+            `;
+        }
+    }))
