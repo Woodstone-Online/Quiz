824f44ae1604bafc04eb0e2c8221c2b711f5647d
PonomareVlad <Git@PonomareVlad.ru>
2021-01-31 04:08:52 +0500

    Conditions stage styles update

--- js/conditions.mjs
+++ js/conditions.mjs
@@ -9,44 +9,34 @@ loadStyles(import.meta.url).then(styles =>
 
         render() {
             return html`
-                <div class="conditions">
-                    <div>
-                        <span class="title">2,7%</span>
-                        <span>Ипотека от 2,7%</span>
-                    </div>
-                    <div>
-                        <span class="title">100%</span>
-                        <span>Учет квартиры до 100%</span>
-                    </div>
-                    <div>
-                        <span class="title">20%</span>
-                        <span>Предоплата 20%, остальные 80% потом</span>
-                    </div>
-                </div>
-                <p>Миссия Woodstone — осуществлять мечту людей о загородном доме.
-                    Помимо строительства мы предлагаем уникальные условия приобретения загородной недвижимости.</p>
-                <p>Какой вариант подходит вам?</p>
+                <h1>Варианты сотрудничества</h1>
                 <div class="plans">
-                    <a href="#contacts" style="background-image: url(/img/1.plans.conditions.png)"
+                    <a href="#contacts"
                        @click="${() => quiz.setAnswer('interest', null, 2) && (quiz.contactSubject = 'Offer') && quiz.sendEvent('LeadButton', 'exchange')}">
                         <h2 class="title">Обмен квартиры на дом</h2>
-                        <p>Выкупим вашу квартиру по рыночной стоимости.</p>
-                        <p>За 4 месяца построим готовый к жизни загородный дом.</p>
-                        <p>Продолжайте жить в квартире, пока идет строительство.</p>
+                        <div>
+                            <p>Выкупим вашу квартиру по рыночной стоимости.</p>
+                            <p>За 4 месяца построим готовый к жизни загородный дом.</p>
+                            <p>Продолжайте жить в квартире, пока идет строительство.</p>
+                        </div>
                     </a>
-                    <a href="#contacts" style="background-image: url(/img/2.plans.conditions.png)"
+                    <a href="#contacts"
                        @click="${() => quiz.setAnswer('interest', null, 1) && (quiz.contactSubject = 'Offer') && quiz.sendEvent('LeadButton', 'mortgage')}">
                         <h2 class="title">Готовый дом за 20%</h2>
-                        <p>Для начала строительства достаточно внести всего 20%.</p>
-                        <p>За 4 месяца мы построим готовый к жизни загородный дом и поможем с оформлением ипотеки от
-                            2.7% годовых.</p>
+                        <div>
+                            <p>Для начала строительства достаточно внести всего 20%.</p>
+                            <p>За 4 месяца мы построим готовый к жизни загородный дом и поможем с оформлением ипотеки от
+                                2.7% годовых.</p>
+                        </div>
                     </a>
-                    <a href="#contacts" style="background-image: url(/img/1.plans.conditions.png)"
+                    <a href="#contacts"
                        @click="${() => quiz.setAnswer('interest', null, 3) && (quiz.contactSubject = 'Offer') && quiz.sendEvent('LeadButton', 'workAgreement')}">
                         <h2 class="title">Поэтапная оплата</h2>
-                        <p>Строительство дома по договору подряда. Вносите оплату поэтапно за результат фактически
-                            выполненных работ.</p>
-                        <p>Классический и наиболее быстрый вариант покупки дома.</p>
+                        <div>
+                            <p>Строительство дома по договору подряда. Вносите оплату поэтапно за результат фактически
+                                выполненных работ.</p>
+                            <p>Классический и наиболее быстрый вариант покупки дома.</p>
+                        </div>
                     </a>
                 </div>
                 <a href="#contacts" class="get-pdf"
